2179eea796ab6ec3bafeb753e76c7bca3af197ef
康子文 <1637750647@qq.com>
2021-06-10 10:09:34 +0800

    初始化后端项目

---
+++ SpringBoot后端/practicaltrain/src/test/java/com/kangzw/practicaltrain/PracticaltrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kangzw.practicaltrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaltrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
