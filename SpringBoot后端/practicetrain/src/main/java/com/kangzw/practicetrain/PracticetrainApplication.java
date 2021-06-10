47541474feec2980c3c312ebc604a790be066e2a
康子文 <1637750647@qq.com>
2021-06-10 10:31:32 +0800

    增加了使用Mongodb数据库的项目

---
+++ SpringBoot后端/practicetrain/src/main/java/com/kangzw/practicetrain/PracticetrainApplication.java
@@ -0,0 +1,13 @@
+package com.kangzw.practicetrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticetrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticetrainApplication.class, args);
+	}
+
+}
---
+++ SpringBoot后端/practicetrain/src/test/java/com/kangzw/practicetrain/PracticetrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kangzw.practicetrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticetrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
